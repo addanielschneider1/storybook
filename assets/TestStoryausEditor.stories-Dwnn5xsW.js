f14562a5d515728f0600387e1db84f27cca9a6f5
ElDel87 <daniel.schneider1@adesso.de>
2025-07-30 10:54:40 +0200

    Updates

---
+++ assets/TestStoryausEditor.stories-Dwnn5xsW.js
@@ -0,0 +1,5 @@
+import{j as m}from"./jsx-runtime-Cf8x2fCZ.js";import"./index-yBjzXJbu.js";const c=()=>m.jsx("div",{dangerouslySetInnerHTML:{__html:"<p>Test</p>"}});c.__docgenInfo={description:"",methods:[],displayName:"TestStoryausEditor"};const p="<p>Test</p>",u={title:"Template-Editor/TestStoryausEditor",component:c},e={},t={render:()=>m.jsx("div",{dangerouslySetInnerHTML:{__html:p}})};var r,o,s;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(s=(o=e.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};var a,n,d;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: () => <div dangerouslySetInnerHTML={{
+    __html: html
+  }} />
+}`,...(d=(n=t.parameters)==null?void 0:n.docs)==null?void 0:d.source}}};const S=["ReactStory","HtmlStory"];export{t as HtmlStory,e as ReactStory,S as __namedExportsOrder,u as default};
